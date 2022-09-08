9c389f51a75aadc4c8885b82dea9439024bf8cc0
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-09 00:58:28 +0300

    Task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-*main func started here
-*main func ended here
-*Return (1)
-*
-*/
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful

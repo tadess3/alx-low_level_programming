0e2876027525a96332ba0e9fa29fb91fe0fb3cfd
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-08 23:17:06 +0300

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
-int main()
+
+/** 
+  * main - Entry point of the program 
+  * takes no arg. 
+  * 
+  * Des: used puts() func to print string to stdout 
+  * Return: function returns 0 always(success) 
+  */
+int main(void)/* main func */
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return(0);
+puts ("\"Programming is like building a multilingual puzzle");
+return(0);
 }

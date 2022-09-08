e5805f7b72c68733c5442feb5165877fc7f126fd
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-09 02:25:13 +0300

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,23 @@
 #include<stdio.h>
+
 /**
-* main - Entry point
-* Return: 0
+* *main func started here
+* *
+* *Return:Always (0)
+*
 */
 
-int main(void)
+int main()
 {
-char d;
-int a;
-long int b;
-long long int c; 
-float f; 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0); 
+int intType;
+float floatType;
+long int liType;
+char charType;
+long long int lliType;
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(liType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(lliType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+return 0;
 }

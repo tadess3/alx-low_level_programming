726276ef835804cb4a26014d7e4ea0936160679e
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-09 00:32:10 +0300

    Task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 
 /**
 *main func started here
@@ -8,6 +9,6 @@
 */
 int main(void)
 {
-fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful" - Dora Korpar, 2015-10-19"));
 return (1);
 }

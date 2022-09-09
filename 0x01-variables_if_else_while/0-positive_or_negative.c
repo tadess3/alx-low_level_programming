80bd2fab8c4a48bd9c772dd3f9897d88259e7569
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-09 22:24:01 +0300

    Task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,11 +7,11 @@
 *
 * Return: Always 0 (Success)
 */
-int main(void)
+int main(void)
 {
-int n;
+int n;
 srand(time(0)); 
-n = rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 if (n>0)
 {
 printf("%i is positive\n", n);

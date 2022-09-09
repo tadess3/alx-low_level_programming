7fdfb569b943d905f58fd68a1b85fc91861e4e48
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-09 22:59:03 +0300

    Tasj
    Task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,21 @@
-#include<stdio.h>
-
-/**
-* main - Prints the alphabetic
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/** 
+* main - Entry point for the program
+* takes no arguments, all statements are in the main function
 *
-* Return: Always (Success)
+* Description: Program that prints alphabets in lower case
+* Return: program always returns 0(success)
 */
 int main(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
+char c = 'a'; 
+while (c <= 'z')
+{
 putchar(c);
+c++;
+}
 putchar('\n');
 return (0); 
 }

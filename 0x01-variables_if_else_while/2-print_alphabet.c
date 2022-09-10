5b943e2fbafab920b961d9bb6f7fd2d6f211d80f
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-11 00:17:54 +0300

    Task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,18 @@
-#include<stdlib.h>
-#include<time.h>
 #include<stdio.h>
 
 /**
-* main - entry point, returns zero, positive, negative based on a number
+*main - entry point
 *
-* Return: always 0
+*Return: always 0
 */
-int main(void)
+int main(void)
 {
-        int n;
- 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-if (n == 0)
-printf("%d is zero\n", n);
-else if (n > 0)
-printf("%d is positive\n", n);
-else
-printf("%d is negative\n", n);
-return (0);
+int c;
+
+for(c = 97; c <= 122; c++)
+{
+putchar(c);
+}
+putchar(10);
+return (0);
 }

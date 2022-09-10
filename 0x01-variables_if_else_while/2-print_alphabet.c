84a3039b33a8f7e18828d587a313296812047410
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-10 22:55:12 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
 *
 * Return: always 0
 */
-int main()
+int main(void)
 {
-         int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+        int n;
+ 
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
 if (n == 0)
 printf("%d is zero\n", n);
 else if (n > 0)

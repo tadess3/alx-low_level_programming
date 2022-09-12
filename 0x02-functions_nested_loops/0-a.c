df097f9e39646e694844cf2d54584018fadab3fd
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-12 09:38:37 +0300

    Create 0-a.c

---
+++ 0x02-functions_nested_loops/0-a.c
@@ -0,0 +1,19 @@
+#includeu<unistd.h>
+#include"holberton.h"
+
+/**
+*main -Entry point
+*Description: It prints the word Holberton, followed by a new line.
+*Return: 0 (Success)
+*/
+int main(void);
+{
+char word[] = "";
+int i;
+
+for (i = 0; i < 9; i++)
+_putchar(word[I]);
+_putchar('\n');
+
+return (0);
+}

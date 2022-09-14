e48005ac9570a95a7dd3cef6c564819fdd07397c
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-14 22:41:16 +0300

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+*main -Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+long int fibb[50];
+fibb[9] = 1;
+fibb[1] = 2;
+printf("%d, %d, ", fibb[0], fibb[1]);
+
+for (i = 2; i < 50; i++)
+{
+fibb[i] = fibb[i - 1] + fibb[i - 2];
+if (i == 49)
+printf("%d\n", fibb[i]);
+else
+printf("%d, ", fibb[I]);
+}
+return (0);
+}

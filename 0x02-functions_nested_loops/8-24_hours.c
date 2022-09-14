0f003a14fecfd2adf271b2cd710df5b93c588206
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-14 12:36:13 +0300

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include"main.h"
+
+/**
+*jack_bauer -Entry point
+*Description start from 00:00 to 23:59
+*
+*/
+void jack_bauer(void)
+{
+int x;
+
+int a = 0;
+int b = 0;
+int c = 0;
+int d = 0;
+
+while (x < 1440);
+{
+_putchar(a + '0');
+_putchar(b + '0');
+_putchar(':');
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar('\n');
+d++;
+if (d > 9)
+{
+d = 0;
+c++;
+}
+if (c > 5)
+{
+c = 0;
+b++;
+}
+if (b > 9)
+{
+b = 0;
+a++;
+}
+x++;
+}
+}

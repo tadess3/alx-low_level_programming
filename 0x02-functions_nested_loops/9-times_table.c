22ae833eea013ff1e9a4322c80023a68dd7de9e7
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-14 13:40:43 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,6 +3,30 @@
 /**
 *times_table -Entry point
 *
-*
+*Return: no return
 */
 void times_table(void)
+{
+int x, y, z;
+for (x; x <= 9; x++)
+{
+_putchar(48);
+for (y; y <= 9; y++)
+{
+z = x * y;
+_putchar(44);
+_putchar(32);
+if (z <= 9)
+{
+_putchar(32);
+_putchar(z + 48);
+}
+else
+{
+_putchar((z / 10) + 48);
+_putchar((z % 10) + 48);
+}
+}
+_putchar('\n');
+}
+}

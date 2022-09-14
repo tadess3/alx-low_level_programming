8bdffb8813fee38cb0cc9c50ea69ceb12ae06262
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-14 22:55:38 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,18 @@
 int main(void)
 {
 int i;
-int fibb[50];
+long int fibb[50];
 fibb[0] = 1;
 fibb[1] = 2;
-printf("%d, %d, ", fibb[0], fibb[1]);
+printf("%ld, %ld, ", fibb[0], fibb[1]);
 
 for (i = 2; i < 50; i++)
 {
 fibb[i] = fibb[i - 1] + fibb[i - 2];
 if (i == 49)
-printf("%d\n", fibb[i]);
+printf("%ld\n", fibb[i]);
 else
-printf("%d, ", fibb[i]);
+printf("%ld, ", fibb[i]);
 }
 return (0);
 }

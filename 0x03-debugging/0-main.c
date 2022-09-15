e1ee37e8e55e82cf6f56baf722bab6b1209ea535
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-15 10:06:38 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
-*main - tests function that prints if integer is positive or negative
+*main -Entry point
 *Description: It prints either positive or negative
 *Return: 0
 */

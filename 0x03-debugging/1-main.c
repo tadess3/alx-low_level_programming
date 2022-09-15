954383dea9aa9d150f9bc93e7b0d65775fa47691
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-15 10:14:16 +0300

    Create 1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+*main -Entry point
+*Return: 0
+*/
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+int i;
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}

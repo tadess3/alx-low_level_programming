556a3b223e0cf91247c489cfbbf176c8bed2e807
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-16 23:37:09 +0300

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+*print_line -Entry point.
+*@n: from parameter.
+*Return: no return.
+*/
+void print_line(void)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}

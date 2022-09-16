c060109ddccb3f4ec75b4474f35ecfe552bda5b7
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-16 22:58:51 +0300

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+*more_numbers -Entry point
+*10 times.
+*Return: no return.
+*/
+void more_numbers(void)
+{
+int c, i;
+for (i = 0; i < 10; i++)
+{
+for (c = 0; c < 15; c++)
+{
+if (c >= 10)
+_putchar((c % 10) + 48);
+_putchar((c / 10) + 48);
+}
+_putchar('\n');
+}
+}

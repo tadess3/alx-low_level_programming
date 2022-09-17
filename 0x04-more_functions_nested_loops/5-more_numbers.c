cd61ae969d19581dcf4734f4ccab90d46b255a62
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-17 19:43:27 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,8 +14,8 @@ for (i = 0; i < 10; i++)
 for (c = 0; c < 15; c++)
 {
 if (c >= 10)
-_putchar((c % 10) + 48);
-_putchar((c / 10) + 48);
+_putchar(c / 10 + 48);
+_putchar(c % 10 + 48);
 }
 _putchar('\n');
 }

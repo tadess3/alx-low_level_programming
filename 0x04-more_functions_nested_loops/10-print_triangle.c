957148e389c9db68ef78913ff7c9d9642555d362
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-17 19:38:35 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,21 +7,24 @@
 */
 void print_triangle(int size)
 {
-int i, j;
-for (i = 0; i < size; i++)
+if (size <= 0)
 {
-for (j = 1; j < (size - 1); j++)
+_putchar('\n');
+}
+else
+{
+int i, j;
+for (i = 1; i <= size; i++)
+{
+for (j = i; j < size; j++)
 {
 _putchar(' ');
 }
-for (j--; j < size; j++)
+for (j = 1; j <= i; j++)
 {
-_putchar(35);
+_putchar('#');
 }
-if (i < (size - 1))
-{
 _putchar('\n');
 }
 }
-_putchar('\n');
 }

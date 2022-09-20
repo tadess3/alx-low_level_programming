a0dbd66a5ba08bd5e74739b8074d57336e0b2cb5
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-21 01:21:16 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,12 +9,13 @@
 */
 void print_array(int *a, int n)
 {
-int i = 0;
-for (; i < n; i++)
+int i;
+for (i = 0; i < n; i++)
 {
-printf("%d", *(a + i));
-if (i != (n - 1))
-printf(", ");
+if (i == 0)
+printf("%d", a[i]);
+else
+printf(", %d", a[i]);
 }
 printf("\n");
 }

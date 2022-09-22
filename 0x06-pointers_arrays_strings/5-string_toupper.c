ba5baa88fef9f6bd7dbe8c373eeabb524254c427
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-09-22 19:26:04 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,11 @@
 */
 char *string_toupper(char *s)
 {
-int i = 0;
-while (*(s + i) != '\0')
+int i;
+for (i = 0; s[i] != '\0'; i++)
 {
-if ((*(s + i) >= 97) && (*(s + i) <= 122))
-*(s + i) = *(s + i) - 32;
-i++;
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 32;
 }
 return (s);
 }

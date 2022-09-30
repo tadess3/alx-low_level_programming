e7a55a8e70f1c089ac1e8c14cf1c15724e9321be
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-10-01 00:02:59 +0300

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,31 +1,15 @@
-#include "main.h" 
+#include "main.h"
 
-  
-
- /** 
-
-  * _isupper -  checks for uppercase character. 
-
-  * @c: c -  Variable 
-
-  * Return: Always 0. 
-
-  */ 
-
- int _isupper(int c) 
-
- { 
-
-  
-
-         if (c >= 65 && c <= 90) 
-
-         { 
-
-                 return (1); 
-
-         } 
-
-         return (0); 
-
- }
+/**
+*_isupper -  checks for uppercase character.
+*@c: c -  Variable
+*Return: Always 0.
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}

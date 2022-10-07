fb3fee9ba26a1cd83b07848f66ee06d8c66888bc
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-10-07 22:10:28 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,19 +1,10 @@
-#ifndef _HOLBERTON_H_ 
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
- #define _HOLBERTON_H_ 
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-  
-
- void *malloc_checked(unsigned int b); 
-
- char *string_nconcat(char *s1, char *s2, unsigned int n); 
-
- void *_calloc(unsigned int nmemb, unsigned int size); 
-
- int *array_range(int min, int max); 
-
- void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size); 
-
-  
-
- #endif
+#endif

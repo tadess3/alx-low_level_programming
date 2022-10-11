52af410382893dfe19f88d7675df4a9b4afb2b86
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-10-11 23:59:04 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,61 +1,28 @@
-#ifndef DOG_H 
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
 
- #define DOG_H 
+/**
+*struct dog - Details of a dog.
+*@name: Pointer to dog's name.
+*@age: Age of dog.
+*@owner: Pointer to owner's name.
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
 
-  
-
- #include <stdio.h> 
-
- #include <stdlib.h> 
-
-  
-
- /** 
-
-  * struct dog - Details of a dog. 
-
-  * @name: Pointer to dog's name. 
-
-  * @age: Age of dog. 
-
-  * @owner: Pointer to owner's name. 
-
-  */ 
-
- struct dog 
-
- { 
-
-         char *name; 
-
-         float age; 
-
-         char *owner; 
-
- }; 
-
-  
-
- /* 
-
-  * dog_t - Synonym for struct dog. 
-
-  */ 
-
- typedef struct dog dog_t; 
-
-  
-
- char *_strdup(char *str); 
-
- void init_dog(struct dog *d, char *name, float age, char *owner); 
-
- void print_dog(struct dog *d); 
-
- dog_t *new_dog(char *name, float age, char *owner); 
-
- void free_dog(dog_t *d); 
-
-  
-
- #endif /* for DOG_H */
+/*
+*dog_t - Synonym for struct dog.
+*/
+typedef struct dog dog_t;
+char *_strdup(char *str);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* for DOG_H */

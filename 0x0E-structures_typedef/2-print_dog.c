124e5c241e74377a534a7802d2cc0fd1a4c8995f
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-10-11 23:53:50 +0300

    Create 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h" 
+
+  
+
+ /** 
+
+  * print_dog - Prints a struct dog. 
+
+  * @d: Pointer to variable of type struct dog. 
+
+  * 
+
+  * Return: No specified return value. 
+
+  */ 
+
+ void print_dog(struct dog *d) 
+
+ { 
+
+         if (!d) 
+
+                 return; 
+
+  
+
+         if (!(d->name)) 
+
+                 d->name = "(nil)"; 
+
+  
+
+         if (!(d->owner)) 
+
+                 d->owner = "(nil)"; 
+
+  
+
+         printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner); 
+
+ }

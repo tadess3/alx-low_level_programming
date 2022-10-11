f84e984c438b31202044ef0997a2ef6991b05d2c
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-10-11 23:55:39 +0300

    Create 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,37 @@
+#include "dog.h" 
+
+  
+
+ /** 
+
+  * free_dog - Frees dogs. 
+
+  * @d: Pointer to type dog_t. 
+
+  * 
+
+  * Return: No return value. 
+
+  */ 
+
+ void free_dog(dog_t *d) 
+
+ { 
+
+         if (d) 
+
+         { 
+
+                 if (d->name) 
+
+                         free(d->name); 
+
+                 if (d->owner) 
+
+                         free(d->owner); 
+
+                 free(d); 
+
+         } 
+
+ }

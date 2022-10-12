8d246cbe4e8377b0708d124369749f433138c7e1
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-10-12 21:37:44 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,19 +1,10 @@
-#ifndef F_POINTERS_H 
+#ifndef F_POINTERS_H
+#define F_POINTERS_H
 
- #define F_POINTERS_H 
+#include <stdio.h>
 
-  
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
- #include <stdio.h> 
-
-  
-
- void print_name(char *name, void (*f)(char *)); 
-
- void array_iterator(int *array, size_t size, void (*action)(int)); 
-
- int int_index(int *array, int size, int (*cmp)(int)); 
-
-  
-
- #endif /* for F_POINTERS_H */
+#endif /* for F_POINTERS_H */

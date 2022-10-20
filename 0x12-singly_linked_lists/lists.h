c284117f665c5976dca9d354472ba20ac400c74f
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-10-20 23:46:24 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,10 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <stdio.h>
+
 /**
 *struct list_s - singly linked list
 *@str: string - (malloc'ed string)
@@ -20,6 +20,7 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
+
 unsigned int _strlen(const char *str);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
@@ -27,4 +28,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 void first(void) __attribute__((constructor));
+
 #endif

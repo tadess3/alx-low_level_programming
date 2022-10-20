e11df27b1fab9a37658f771b89f5838695204662
Tadesse Belachew <111916727+tadess3@users.noreply.github.com>
2022-10-20 23:17:19 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,17 +3,11 @@
 void _construct(void) __attribute__((constructor));
 
 /**
-
- * _construct - constructor function
-
- */
-
+*_construct - constructor function
+*/
 void _construct(void)
-
 {
-
-	char line1[] = "You're beat! and yet, you must allow,";	char line2[] = "I bore my house upon my back!";
-
-	printf("%s\n%s\n", line1, line2);
-
+char line1[] = "You're beat! and yet, you must allow,";
+char line2[] = "I bore my house upon my back!";
+printf("%s\n%s\n", line1, line2);
 }
